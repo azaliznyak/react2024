bc01ab41f0e1c3049824560092ca1c1b3eb39894
andrianazaliznak <andrianazaliznyak@gmail.com>
2024-02-03 22:54:07 +0100

    updated

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
-const Character = () => {
+const Character = ({character}) => {
+    const {name,status,species,img,genre}=character
     return (
         <div>
+            <h1>{name}</h1>
+            <h4>{status} - {species}</h4>
+            <h5>{genre}</h5>
+            <img src={img} alt={name}/>
             
         </div>
     );
