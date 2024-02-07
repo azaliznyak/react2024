f358f33b4732e3b3f7ec2d617913c8a33b4d36ed
andrianazaliznak <andrianazaliznyak@gmail.com>
2024-02-07 19:03:02 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-import {useEffect} from 'react'
+
 import {Users} from './components/Users'
 
 const App = () => {
-    useEffect()
+
+
     return (
         <div>
             <Users/>
