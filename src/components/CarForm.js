589ec64b4e8f8d50497f29eddfa6af3f7fd4a9b3
andrianazaliznak <andrianazaliznyak@gmail.com>
2024-02-14 21:05:41 +0100

    update

--- src/components/CarForm.js
+++ src/components/CarForm.js
@@ -1,10 +1,13 @@
 import React, {useEffect} from 'react';
 import {useForm} from "react-hook-form";
 import {carService} from "../services/carService";
+import Joi from "joi";
+import {joiResolver} from "@hookform/resolvers/joi";
+import {carValidator} from "../validators/carValidator";
 
 const CarForm = ({setTrigger, carForUpdate,setCarForUpdate}) => {
 
-    const {register,reset,handleSubmit, setValue, formState:{errors,isValid}}=useForm({mode:'all'});
+    const {register,reset,handleSubmit, setValue, formState:{errors,isValid}}=useForm({mode:'all', resolver:joiResolver(carValidator)});
 
     const save = async (car) => {
         await carService.create(car)
@@ -29,24 +32,11 @@ const CarForm = ({setTrigger, carForUpdate,setCarForUpdate}) => {
     }
     return (
         <form onSubmit={handleSubmit(carForUpdate? update:save)}>
-            <input type="text" placeholder={'brand'} {...register('brand',{
-                pattern:{
-                    value:/^[a-zA-Zа-яА-яёЁіІїЇєЄҐґ]{1,20}$/,
-                    message:'min 1 max 20 characters'
-                }
-            })}/>
+            <input type="text" placeholder={'brand'} {...register('brand')}/>
 
-            <input type="text" placeholder={'year'} {...register('year', {
-                valueAsNumber:true,
-                min:{value:1990, message:'min 1990'},
-                max:{value:new Date().getFullYear(), message:'max currant year'}
-            })}/>
+            <input type="text" placeholder={'year'} {...register('year', { valueAsNumber:true })}/>
 
-            <input type="text" placeholder={'price'} {...register('price', {
-                valueAsNumber:true,
-                min:{value:1, message:'min 1'},
-                max:{value:1000000,message:'max 1000000'}
-            })}/>
+            <input type="text" placeholder={'price'} {...register('price', {valueAsNumber:true})}/>
             <button disabled={!isValid}>{carForUpdate?'update':'save'}</button>
             {errors.brand&&<div>{errors.brand.message}</div>}
             {errors.year&&<div>{errors.year.message}</div>}
