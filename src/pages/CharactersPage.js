72e93458594308907c8bf049be74d69a62c353ca
andrianazaliznak <andrianazaliznyak@gmail.com>
2024-02-26 20:34:41 +0200

    updated

--- src/pages/CharactersPage.js
+++ src/pages/CharactersPage.js
@@ -1,7 +1,6 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {Characters} from "../components";
-import {useParams} from "react-router-dom";
-import {characterService} from "../servers";
+
 
 const CharactersPage = () => {
 
